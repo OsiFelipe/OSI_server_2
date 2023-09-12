24f80ee272a7c2e4915f4dddd9dccf825df6c175
FelipeRUnosquare <jramirez@odsep.com>
2023-09-11 19:31:57 -0500

    added parse apps

---
+++ parse-apps.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "Sales App",
+      script: "server.js",
+      watch: true,
+      merge_logs: true,
+      cwd: "/home/ubuntu/projects/OSI_server_2",
+    },
+  ],
+};
