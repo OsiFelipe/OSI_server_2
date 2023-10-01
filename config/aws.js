4ba6a83eb426fb79fcaa548c069d94f8f8032c22
FelipeRUnosquare <jramirez@odsep.com>
2023-09-30 20:50:11 -0500

    key deleted

--- config/aws.js
+++
@@ -1,8 +0,0 @@
-const AWS = require("aws-sdk");
-
-const s3 = new AWS.S3({
-  accessKeyId: "AKIA2KFZC7FUE3DWUZA3",
-  secretAccessKey: "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",
-  region: "US East (N. Virginia) us-east-1",
-});
-module.exports = { s3 };
