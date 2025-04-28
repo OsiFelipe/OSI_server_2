1807e8ae3171c0b2d7cef5ed5be195ec8296b2db
felipe <a123456789@Felipes-MacBook-Pro.local>
2025-04-28 16:42:45 -0500

    adding enhancements

--- server.js
+++ server.js
@@ -13,19 +13,9 @@ const PORT = process.env.DB_PORT || 8080;
 const crt = process.env.CERT_CRT;
 const key = process.env.CERT_KEY;
 
-// var corsOptions = {
-//   origin: [
-//     "http://localhost:3000",
-//     "https://uat.osidesigner.com",
-
-//     "https://osidesigner.com",
-//   ],
-//   exposedHeaders: ["X-Total-Records", "X-Total-Pages", "X-Current-Page"],
-// };
-
 app.use(
   cors({
-    origin: ["http://localhost:3000"], // Allow only this origin
+    origin: ["https://osidesigner.com"], // Allow only this origin
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed request headers
     exposedHeaders: [
@@ -47,7 +37,7 @@ app.use(function (req, res, next) {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://osidesigner.com");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
