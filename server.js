0856804eb5b6435da2ddedf617faa682986b20f8
felipe <a123456789@Felipes-MacBook-Pro.local>
2025-05-01 16:11:27 -0500

    adding proposal client and chemical tracker modules

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const crt = process.env.CERT_CRT;
 const key = process.env.CERT_KEY;
 app.use(
   cors({
-    // origin: ["https://osidesigner.com"], // Allow only this origin
-    origin: ["http://localhost:3000"], // Allow only this origin
+    origin: ["https://osidesigner.com"], // Allow only this origin
+    // origin: ["http://localhost:3000"], // Allow only this origin
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed request headers
     exposedHeaders: [
@@ -37,8 +37,8 @@ app.use(function (req, res, next) {
 });
 
 app.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "https://osidesigner.com");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://osidesigner.com");
+  //res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
@@ -83,21 +83,21 @@ db.sequelize
   .sync({ force: false })
   .then(() => {
     console.log("Synced db.");
-    app.listen(PORT, () => {
-      console.log(`🚀  Server is running on port ${PORT}.`);
-    });
+    // app.listen(PORT, () => {
+    //   console.log(`🚀  Server is running on port ${PORT}.`);
+    // });
 
-    // https
-    //   .createServer(
-    //     {
-    //       cert: fs.readFileSync(crt),
-    //       key: fs.readFileSync(key),
-    //     },
-    //     app,
-    //   )
-    //   .listen(PORT, function () {
-    //     console.log(`App listening on port ${PORT}`);
-    //   });
+    https
+      .createServer(
+        {
+          cert: fs.readFileSync(crt),
+          key: fs.readFileSync(key),
+        },
+        app,
+      )
+      .listen(PORT, function () {
+        console.log(`App listening on port ${PORT}`);
+      });
   })
   .catch((err) => {
     console.log("Failed to sync db: " + err.message);
